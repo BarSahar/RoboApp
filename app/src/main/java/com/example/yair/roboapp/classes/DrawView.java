e6ec409c7e0986697f3a396dc156ff7880b98778
Avi Hazan <avihazan22@gmail.com>
2017-04-22 08:13:57 +0300

    dgrfh

---
+++ app/src/main/java/com/example/yair/roboapp/classes/DrawView.java
@@ -0,0 +1,32 @@
+package com.example.yair.roboapp.classes;//package com.example.yair.roboapp.classes;
+//
+///**
+// * Created by aviha on 21-Apr-17.
+// */
+//
+//public class DrawView {
+//}
+//
+//
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class DrawView extends View {
+    Paint paint = new Paint();
+
+    public DrawView(Context context) {
+        super(context);
+        paint.setColor(Color.BLACK);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawLine(0, 0, 20, 20, paint);
+        canvas.drawLine(20, 0, 0, 20, paint);
+    }
+
+}
